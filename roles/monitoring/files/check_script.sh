5d5c1cf924ae1f01b8135bfacf279613c82c1c77
juliovp01 <juliovp01@gmail.com>
2016-01-02 17:14:35 -0600

    Fix indentation for JSON file.

--- roles/monitoring/files/check_script.sh
+++ roles/monitoring/files/check_script.sh
@@ -1,12 +1,13 @@
 for i in $(systemctl | egrep 'openstack|neutron' | grep -v losetup | awk '{print $1}'); do cat << EOF > /etc/sensu/conf.d/check-$i.json; done
 {
-  "checks": {
-     "$i": {
-      "command": "systemctl status $i",
-      "standalone": true,
-      "interval": 10,
-      "handler": "pager"
-    }
-  }
+  "checks": {
+    "$i": {
+      "command": "systemctl status $i",
+      "standalone": true,
+      "interval": 60,
+      "handler": "pager",
+      "subscribers": [ "ALL" ]
+    }
+  }
 }
 EOF
